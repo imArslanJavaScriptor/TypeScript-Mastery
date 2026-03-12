204d94315408c1f4fb41f64261c490c0334daa34
Muhammad Arslan <imarslan7061@gmail.com>
2026-03-12 09:51:13 +0500

    Update 11-utils-type.ts

--- src/11-utils-type.ts
+++ src/11-utils-type.ts
@@ -1,3 +1,15 @@
-interface Adress {
-  street: string
+interface Geo {
+  lat: string,
+  lng: string
+}
+
+interface Company {
+  name: string,
+  catchPhrase: string,
+  bs: string
+}
+
+interface User {
+  id: number,
+  name: string
 }
